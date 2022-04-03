790e20c7efdf6b66e7781735b3cf0d5f3a37c08a
CamKE <37081579+CamKE@users.noreply.github.com>
2022-04-03 12:43:47 +0100

    hover tip functionality added for options
    followed tut to create

--- Assets/Scripts/HovertipManager.cs
+++ Assets/Scripts/HovertipManager.cs
@@ -1,24 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
 
-public class HovertipManager : MonoBehaviour
+// Credit to Comp-3 Interative for the tutorial
+public class HoverTipManager : MonoBehaviour
 {
-    public UnityEngine.UIElements.TextElement text;
+    public TextMeshProUGUI tipText;
     public RectTransform tipWindow;
-    
 
+    public static Action<string, Vector2> onMouseHover;
+    public static Action onMouseLoseFocus;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        hideTip();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        onMouseHover += showTip;
+        onMouseLoseFocus += hideTip;
+    }
+
+    private void OnDisable()
+    {
+        onMouseHover -= showTip;
+        onMouseLoseFocus -= hideTip;
+    }
+
+    private void showTip(string tip, Vector2 mousePos)
+    {
+        tipText.text = tip;
+        tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 200 ? 200 : tipText.preferredWidth, tipText.preferredHeight);
+        Debug.Log(tipWindow.sizeDelta);
+        tipWindow.gameObject.SetActive(true);
+        tipWindow.transform.position = new Vector2(mousePos.x + tipWindow.sizeDelta.x * 0.6f, mousePos.y);
+    }
+
+    private void hideTip()
+    {
+        tipText.text = default;
+        tipWindow.gameObject.SetActive(false);
     }
 }
